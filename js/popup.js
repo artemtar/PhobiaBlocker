7e8fe829628b01544e95fadfd6985ae0c77c1d59
Tarasov Artem[ TarasovArtem ] <atarasov@lycorp.co.jp>
2026-01-30 23:13:44 +0900

    remove tagify

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,74 @@
 $(() => {
+    let targetWords = []
 
-    let newTargetWord = document.querySelector('.addedWordArea'),
-        tagify = new Tagify(newTargetWord, {
-            pattern: /^.{0,30}$/,
-            maxTags: 20,
+    // Render tags
+    function renderTags() {
+        const container = $('#tags-container')
+        container.empty()
+
+        targetWords.forEach((word, index) => {
+            const tag = $('<span>')
+                .addClass('tag')
+                .text(word)
+
+            const removeBtn = $('<button>')
+                .addClass('tag-remove')
+                .html('<svg width="12" height="12" viewBox="0 0 12 12" fill="none"><path d="M9 3L3 9M3 3L9 9" stroke="currentColor" stroke-width="2" stroke-linecap="round"/></svg>')
+                .attr('data-index', index)
+                .click(function() {
+                    removeTag($(this).attr('data-index'))
+                })
+
+            tag.append(removeBtn)
+            container.append(tag)
         })
+    }
 
+    // Add tag
+    function addTag(word) {
+        word = word.trim()
+        if (!word || word.length > 30) return
+        if (targetWords.includes(word)) return
+        if (targetWords.length >= 20) return
+
+        targetWords.push(word)
+        updateStorage()
+        renderTags()
+        $('#word-input').val('')
+    }
+
+    // Remove tag
+    function removeTag(index) {
+        targetWords.splice(index, 1)
+        updateStorage()
+        renderTags()
+    }
+
+    // Update storage
+    function updateStorage() {
+        chrome.storage.sync.set({ targetWords: targetWords })
+    }
+
+    // Load initial tags
+    chrome.storage.sync.get('targetWords', (storage) => {
+        if (storage.targetWords) {
+            targetWords = storage.targetWords
+            renderTags()
+        }
+    })
+
+    // Add button click
     $('#addTagBtn').click(() => {
-        tagify.addEmptyTag()
+        const word = $('#word-input').val()
+        addTag(word)
+    })
+
+    // Enter key to add
+    $('#word-input').keypress((e) => {
+        if (e.which === 13) {
+            const word = $('#word-input').val()
+            addTag(word)
+        }
     })
 
     $('#unblurBtn').click(() => {
@@ -31,23 +92,6 @@ $(() => {
                 })
             })
     })
-
-    let addButton = $('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00447c" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>')
-    $('#addTagBtn').html(addButton)
-
-    chrome.storage.sync.get('targetWords', (storage) => {
-        tagify.addTags(storage['targetWords'])
-    })
-
-
-    let updateTargetWords = () => {
-        let newTargetWords = tagify.value.map(wordElement => wordElement['value'])
-        chrome.storage.sync.set({ targetWords: newTargetWords })
-    }
-
-    tagify.on('edit', updateTargetWords)
-        .on('remove', updateTargetWords)
-        .on('add', updateTargetWords)
 
     $(document).on('input', '#blurRange', () => {
         let blurValueAmount = $('#blurRange').val()
@@ -87,7 +131,7 @@ $(() => {
             })
         chrome.storage.sync.set({ 'blurIsAlwaysOn': $('#blurIsAlwaysOn-switch').prop('checked')})
     })
-    
+
     chrome.storage.sync.get('blurValueAmount', (storage) => {
         if (storage.blurValueAmount) {
             $('#blurRange').val(storage.blurValueAmount)
@@ -96,7 +140,7 @@ $(() => {
 
     let arrorRightIcon = $('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16"><path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/></svg>')
     let arrorDownIcon = $('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16"><path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/></svg>')
-    
+
     chrome.storage.sync.get('supportedWordsCollapsed', (storage) => {
         if (storage.supportedWordsCollapsed) {
             $('#btn-supported-words').html(arrorRightIcon)
@@ -117,7 +161,7 @@ $(() => {
             chrome.storage.sync.set({ 'supportedWordsCollapsed': false })
         }
     })
-    
+
     /**
      * on first start these words will be used as example
     **/
@@ -128,8 +172,11 @@ $(() => {
         'phobiaBlockerEnabled',
         'blurIsAlwaysOn'
     ], (storage) => {
-        if (!storage.targetWords)
+        if (!storage.targetWords) {
             chrome.storage.sync.set({ 'targetWords': defaultTarget })
+            targetWords = defaultTarget
+            renderTags()
+        }
         if (storage.phobiaBlockerEnabled)
             $('#enabled-switch').prop('checked', storage.phobiaBlockerEnabled)
         if (storage.blurIsAlwaysOn)
